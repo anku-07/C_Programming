b7711911c8138ea70886b19cbfd46cead002b01d
anku-07 <adityamondal48765@gmail.com>
2023-12-30 10:33:58 +0530

    Added pointer folder and some pointer related file

---
+++ Pointers/findingOutput.c
@@ -0,0 +1,16 @@
+// Finding Output
+#include <stdio.h>
+int main()
+{
+    int *ptr;
+    int x;
+    ptr = &x;
+    *ptr = 0;
+    printf("X = %d\n", x);
+    printf("*ptr = %d\n", *ptr);
+    *ptr += 5;
+    printf("X = %d\n", x);
+    printf("*ptr = %d\n", *ptr);
+
+    return 0;
+}
---
+++ Pointers/syntax.c
@@ -0,0 +1,18 @@
+// Declaer a pointer.
+#include<stdio.h>
+int main(){
+    int age = 21;
+    int *ptr = &age;
+    int _age = *ptr;
+
+    //printf("%d",_age);
+
+    //Printing address
+    //printf("%p \n",&age);
+    printf("%u \n",&age);
+    printf("%u \n",ptr);
+    printf("%u \n",&ptr);
+
+
+    return 0;
+}
