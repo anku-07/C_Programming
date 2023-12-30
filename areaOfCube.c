c6b112871c6cc23fc08e8c9228eab68df16fc6b4
anku-07 <adityamondal48765@gmail.com>
2023-12-30 20:10:12 +0530

    Added area of cube

---
+++ areaOfCube.c
@@ -0,0 +1,10 @@
+// Write a progarm in c to calculate area of cube.
+#include<stdio.h>
+int main(){
+    int cube,n;
+    printf("Enter value of cube : ");
+    scanf("%d",&n);
+    cube = n*n*n;
+    printf("The value of a cube is %d",cube);
+    return 0;
+}
