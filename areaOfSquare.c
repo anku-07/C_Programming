c8251748f393b87b6d0a4b4814529a08f7c1e2ca
anku-07 <adityamondal48765@gmail.com>
2023-12-30 19:55:26 +0530

    Added file

---
+++ areaOfSquare.c
@@ -0,0 +1,9 @@
+// Write a program in c area of square.
+#include<stdio.h>
+int main(){
+    int side1,side2;
+    printf("Enter the sides : ");
+    scanf("%d%d",&side1,&side2);
+    printf("Area of square is %d",side1 * side2);
+    return 0;
+}
