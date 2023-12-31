89620a1c792271f7c24d3727d2b0cc03042cff29
anku-07 <adityamondal48765@gmail.com>
2023-12-31 11:13:47 +0530

    Added

---
+++ simpleInterest.c
@@ -0,0 +1,8 @@
+/*
+Design a program to calculate simple interest based on user-provided principal amount, rate, and time.
+*/
+#include<stdio.h>
+int main(){
+    float amount,time,rateOfInterest,finalAmount;
+    return 0;
+}
