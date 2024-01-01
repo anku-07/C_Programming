d2c866afbc14dd4c0adeb2df495e23dc0df3ec76
anku-07 <adityamondal48765@gmail.com>
2024-01-01 17:40:54 +0530

    Added simple interset file using c language

--- simpleInterest.c
+++ simpleInterest.c
@@ -1,8 +1,26 @@
 /*
 Design a program to calculate simple interest based on user-provided principal amount, rate, and time.
 */
-#include<stdio.h>
-int main(){
-    float amount,time,rateOfInterest,finalAmount;
+#include <stdio.h>
+
+int main() {
+    float principal, rate, time, simple_interest;
+
+    // Taking user input for principal amount, rate, and time
+    printf("Enter the principal amount: ");
+    scanf("%f", &principal);
+
+    printf("Enter the rate of interest (in percentage): ");
+    scanf("%f", &rate);
+
+    printf("Enter the time (in years): ");
+    scanf("%f", &time);
+
+    // Calculating simple interest
+    simple_interest = (principal * rate * time) / 100;
+
+    // Displaying the calculated simple interest
+    printf("Simple Interest = %.2f\n", simple_interest);
+
     return 0;
 }
