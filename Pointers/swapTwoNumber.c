93ee3b87f751d50d1bb9d83246fc3811f3b79273
anku-07 <adityamondal48765@gmail.com>
2024-01-03 23:47:29 +0530

    call by refanrence using function

--- Pointers/swapTwoNumber.c
+++ Pointers/swapTwoNumber.c
@@ -1,13 +1,20 @@
 // Swap 2 numbers, A & B;
 void Swap(int a, int b);
-
+void _swap(int *a, int *b);
 #include<stdio.h>
 int main(){
     int x = 3, y = 5;
-    Swap(x,y);
+    _swap(&x,&y);
     printf("x = %d & y = %d\n",x,y);
     return 0;
 }
+// Call by refarence
+void _swap(int *a, int *b){
+    int p = *a;
+    *a = *b;
+    *b = p;
+}
+// Call by value
 void Swap(int a, int b){
     int t = a;
     a = b;
