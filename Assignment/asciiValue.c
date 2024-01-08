dfcd72283a9386f79da523a417f179117852a56f
anku-07 <adityamondal48765@gmail.com>
2024-01-08 23:07:03 +0530

    added ascii value

---
+++ Assignment/asciiValue.c
@@ -0,0 +1,9 @@
+//Print ascii value of the character.
+#include<stdio.h>
+int main(){
+    char ch;
+    printf("Enter an character : ");
+    scanf("%c",&ch);
+    printf("%c ascii value is %d",ch,ch);
+    return 0;
+}
