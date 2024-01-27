e22bfa9a2bcb04e090685b14aea20b012af3369c
anku-07 <adityamondal48765@gmail.com>
2024-01-27 23:22:10 +0530

    output related questions

---
+++ outputRelatedQuestion/Q-1.c
@@ -0,0 +1,11 @@
+// How many times will be the loop be executed?
+#include<stdio.h>
+int main(){
+    int x = 500;
+    while(x <= 500){
+        x = x - 600;
+        if(x < 0)
+        break;
+    }
+    return 0;
+}
