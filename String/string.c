537ef9d1c837eecd75af9859111a21f585397b2b
anku-07 <adityamondal48765@gmail.com>
2024-02-18 00:21:49 +0530

    Add new line

--- String/string.c
+++ String/string.c
@@ -1,12 +1,16 @@
 #include<stdio.h>
 char printString(char arr[]);
 int main(){
-    char name[] = {'A','D','I','T','Y','A','\0'};
-    printString(name);
+    char fname[] = {'A','D','I','T','Y','A','\0'};
+    char lname[] = "MONDAl";
+    printf("Your name is %s %s",fname,lname);
+    // printString(fname);
+    // printString(lname);
     return 0;
 }
 char printString(char arr[]){
     for(int i = 0; arr[i] != '\0'; i++){
         printf("%c",arr[i]);
     }
+    printf("\n");
 }
